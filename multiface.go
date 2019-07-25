b414aa6424a809c90e591c4c62f97fa8432adbf3
André Hänsel <haensel@creations.de>
2019-07-25 21:47:01 +0200

    Add AddFace method
    This adds a method AddFace() to the multiface.Face that can be used to
    add arbitrary (well-behaved) font.Face to the multiface.Face.
    
    I initially omitted this function to make sure no one accidentally uses
    it with Truetype font instead of AddTruetypeFace(). (Those fonts return
    ok = true even when glyphs are missing, so they need to be handled
    differently.)
    
    Now #1 requests this to be usable with BDF fonts, so I added that
    function and also a test with such a font.

--- multiface.go
+++ multiface.go
@@ -1,10 +1,10 @@
 package multiface
 
 import (
+	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/font"
 	"golang.org/x/image/math/fixed"
 	"image"
-	"github.com/golang/freetype/truetype"
 )
 
 type entry struct {
@@ -18,6 +18,14 @@ type Face struct {
 
 func (f *Face) AddTruetypeFace(face font.Face, fnt *truetype.Font) {
 	f.entries = append(f.entries, entry{fnt, face})
+}
+
+// AddFace adds a font.Face to the multiface.Face
+//
+// DO NOT USE this for Truetype fonts.
+// github.com/golang/freetype/truetype.Face methods return ok = true even when the glyph could not be found in the font.
+func (f *Face) AddFace(face font.Face) {
+	f.entries = append(f.entries, entry{nil, face})
 }
 
 func (f *Face) Close() error {
@@ -34,11 +42,11 @@ func (f *Face) Close() error {
 func (f *Face) Glyph(dot fixed.Point26_6, r rune) (dr image.Rectangle, mask image.Image, maskp image.Point, advance fixed.Int26_6, ok bool) {
 	for i := range f.entries {
 		// truetype.faces don't return ok = false for missing glyphs, but it seems when .Index(r) == 0 the glyph is missing
-		if f.entries[i].font != nil && f.entries[i].font.Index(r) == 0 && i < len(f.entries) - 1 {
+		if f.entries[i].font != nil && f.entries[i].font.Index(r) == 0 && i < len(f.entries)-1 {
 			continue
 		}
 		dr, mask, maskp, advance, ok = f.entries[i].face.Glyph(dot, r)
-		if !ok && i < len(f.entries) - 1 {
+		if !ok && i < len(f.entries)-1 {
 			continue
 		}
 		return
@@ -49,11 +57,11 @@ func (f *Face) Glyph(dot fixed.Point26_6, r rune) (dr image.Rectangle, mask imag
 func (f *Face) GlyphBounds(r rune) (bounds fixed.Rectangle26_6, advance fixed.Int26_6, ok bool) {
 	for i := range f.entries {
 		// truetype.faces don't return ok = false for missing glyphs, but it seems when .Index(r) == 0 the glyph is missing
-		if f.entries[i].font != nil && f.entries[i].font.Index(r) == 0 && i < len(f.entries) - 1 {
+		if f.entries[i].font != nil && f.entries[i].font.Index(r) == 0 && i < len(f.entries)-1 {
 			continue
 		}
 		bounds, advance, ok = f.entries[i].face.GlyphBounds(r)
-		if !ok && i < len(f.entries) - 1 {
+		if !ok && i < len(f.entries)-1 {
 			continue
 		}
 		return
@@ -64,11 +72,11 @@ func (f *Face) GlyphBounds(r rune) (bounds fixed.Rectangle26_6, advance fixed.In
 func (f *Face) GlyphAdvance(r rune) (advance fixed.Int26_6, ok bool) {
 	for i := range f.entries {
 		// truetype.faces don't return ok = false for missing glyphs, but it seems when .Index(r) == 0 the glyph is missing
-		if f.entries[i].font != nil && f.entries[i].font.Index(r) == 0 && i < len(f.entries) - 1 {
+		if f.entries[i].font != nil && f.entries[i].font.Index(r) == 0 && i < len(f.entries)-1 {
 			continue
 		}
 		advance, ok = f.entries[i].face.GlyphAdvance(r)
-		if !ok && i < len(f.entries) - 1 {
+		if !ok && i < len(f.entries)-1 {
 			continue
 		}
 		return
@@ -79,20 +87,20 @@ func (f *Face) GlyphAdvance(r rune) (advance fixed.Int26_6, ok bool) {
 func (f *Face) Kern(r0, r1 rune) fixed.Int26_6 {
 	for i := range f.entries {
 		// truetype.faces don't return ok = false for missing glyphs, but it seems when .Index(r) == 0 the glyph is missing
-		if f.entries[i].font != nil && f.entries[i].font.Index(r0) == 0 && i < len(f.entries) - 1 {
+		if f.entries[i].font != nil && f.entries[i].font.Index(r0) == 0 && i < len(f.entries)-1 {
 			continue
 		}
-		if f.entries[i].font != nil && f.entries[i].font.Index(r1) == 0 && i < len(f.entries) - 1 {
+		if f.entries[i].font != nil && f.entries[i].font.Index(r1) == 0 && i < len(f.entries)-1 {
 			continue
 		}
 
 		var ok bool
 		_, ok = f.entries[i].face.GlyphAdvance(r0)
-		if !ok && i < len(f.entries) - 1 {
+		if !ok && i < len(f.entries)-1 {
 			continue
 		}
 		_, ok = f.entries[i].face.GlyphAdvance(r1)
-		if !ok && i < len(f.entries) - 1 {
+		if !ok && i < len(f.entries)-1 {
 			continue
 		}
 
--- multiface_test.go
+++ multiface_test.go
@@ -1,17 +1,18 @@
 package multiface_test
 
 import (
-	"testing"
+	"bytes"
+	"github.com/AndreKR/multiface"
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	"github.com/zachomedia/go-bdf"
 	"golang.org/x/image/font"
 	"image"
-	"github.com/golang/freetype/truetype"
-	"github.com/golang/freetype"
-	"io/ioutil"
-	"github.com/AndreKR/multiface"
 	"image/draw"
-	"os"
 	"image/png"
-	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
 )
 
 func TestMultiface(t *testing.T) {
@@ -60,10 +61,65 @@ func TestMultiface(t *testing.T) {
 	}
 }
 
+func TestBdf(t *testing.T) {
+
+	face := new(multiface.Face)
+
+	opts := &truetype.Options{Size: 20, DPI: 96}
+
+	var fnt *truetype.Font
+	var bdffnt *bdf.Font
+	var fc font.Face
+
+	// Add Terminus font
+	bdffnt = readBdfFont(t, "testdata/ter-u12n.bdf")
+	fc = &bdf.Face{Font: bdffnt}
+	face.AddTruetypeFace(fc, fnt)
+
+	// Add Kanit font, which does have a glyph for ก
+	fnt = readFont(t, "testdata/Kanit-Regular.ttf")
+	fc = truetype.NewFace(fnt, opts)
+	face.AddTruetypeFace(fc, fnt)
+
+	img := image.NewRGBA(image.Rect(0, 0, 50, 50))
+	draw.Draw(img, img.Rect, image.White, image.ZP, draw.Src)
+
+	d := font.Drawer{}
+	d.Dst = img
+	d.Src = image.Black
+	d.Face = face
+	d.Dot = freetype.Pt(10, 25)
+	d.DrawString("กa")
+
+	f, err := os.Create("testdata/output.png")
+	checkErr(t, err)
+	err = png.Encode(f, img)
+	checkErr(t, err)
+	err = f.Close()
+	checkErr(t, err)
+
+	f, err = os.Open("testdata/reference_bdf.png")
+	checkErr(t, err)
+	ref, err  := png.Decode(f)
+	checkErr(t, err)
+
+	if bytes.Compare(ref.(*image.RGBA).Pix, img.Pix) != 0 {
+		t.Fatal("output does not match reference")
+	}
+}
+
 func readFont(t *testing.T, filename string) *truetype.Font {
 	data, err := ioutil.ReadFile(filename)
 	checkErr(t, err)
 	fnt, err := truetype.Parse(data)
+	checkErr(t, err)
+	return fnt
+}
+
+func readBdfFont(t *testing.T, filename string) *bdf.Font {
+	data, err := ioutil.ReadFile(filename)
+	checkErr(t, err)
+	fnt, err := bdf.Parse(data)
 	checkErr(t, err)
 	return fnt
 }
